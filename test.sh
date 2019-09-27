5474a4bebf4eb955ef529d72b9c98928fdb7a1ec
Dennis Jackson <dennis.jackson@cs.ox.ac.uk>
2019-09-27 16:19:26 +0100

    Fix test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
+# wget https://www.antlr.org/download/antlr-4.7.2-complete.jar
+
 rm -r out
-/opt/antlr -o out Tamarin.g4
-cd out
-javac Tamarin*.java
-java org.antlr.v4.gui.TestRig Tamarin theory -gui ../tendermint_subgroups.spthy     
+java -jar antlr-4.7.2-complete.jar -o out Tamarin.g4
+find . -type f -name "*.java" -print | xargs javac -cp antlr-4.7.2-complete.jar
+java -cp antlr-4.7.2-complete.jar:out/ org.antlr.v4.gui.TestRig Tamarin theory -gui tendermint_subgroups.spthy 
