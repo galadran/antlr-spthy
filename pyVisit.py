91d2858fe6e611ed8b9dec8b5ae0c45ba0931287
Dennis Jackson <dennis.jackson@cs.ox.ac.uk>
2019-10-01 16:33:47 +0100

    Updated visitor script

--- pyVisit.py
+++ pyVisit.py
@@ -4,13 +4,18 @@ import sys
 from antlr4 import *
 from out.TamarinruleLexer import TamarinruleLexer
 from out.TamarinruleParser import TamarinruleParser
- 
+from out.TamarinruleVisitor import TamarinruleVisitor
+
 def main(argv):
-    input_stream = FileStream(argv[1])
-    lexer = TamarinruleLexer(input_stream)
-    stream = CommonTokenStream(lexer)
-    parser = TamarinruleParser(stream)
-    tree = parser.rules()
- 
+	input_stream = FileStream(argv[1])
+	lexer = TamarinruleLexer(input_stream)
+	stream = CommonTokenStream(lexer)
+	parser = TamarinruleParser(stream)
+	tree = parser.rules()
+	return tree 
+
 if __name__ == '__main__':
-    main(sys.argv)
+	t = main(sys.argv)
+	l = TamarinruleVisitor()
+	r = l.visit(t)
+	print(r)
