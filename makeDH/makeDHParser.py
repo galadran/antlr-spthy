b9c3e1cdccdcee86997ac4cd7f60be2e25db1539
Dennis Jackson <dennis.jackson@cs.ox.ac.uk>
2019-10-10 17:43:09 +0100

    Tweak makeDH to monkeypatch TamarinParser rather than hardcode it

---
+++ makeDH/makeDHParser.py
@@ -0,0 +1,52 @@
+import TamarinruleParser
+from io import StringIO
+
+def protoOverride(self):
+    if self.getChildCount() == 0:
+        return ""
+    with StringIO() as builder:
+        builder.write("\n")
+        for child in self.getChildren():
+            builder.write(child.getText())
+            if ':' in child.getText():
+                builder.write("\n")
+            if child.getText() == 'rule':
+                builder.write(" ")
+        return builder.getvalue()
+
+TamarinruleParser.TamarinruleParser.ProtoRuleContext.getText = protoOverride
+
+def genericOverride(self):
+    if self.getChildCount() == 0:
+        return ""
+    with StringIO() as builder:
+        for child in self.getChildren():
+            builder.write(child.getText())
+        builder.write("\n")
+        return builder.getvalue()
+
+TamarinruleParser.TamarinruleParser.GenericRuleContext.getText = genericOverride
+
+def termOverride(self,transform=True):
+    if not transform:
+        with StringIO() as builder:
+            for child in self.getChildren():
+                if type(child) == type(self):
+                    builder.write(child.getText(transform=False))
+                else:
+                    builder.write(child.getText())
+            return builder.getvalue()
+    if transform:
+        from .transformElements import transformTerm
+        o = self.getText(transform=False)
+        r = transformTerm(self)
+        if 'element' in r :
+            pass
+            #print(o + ' -> ' + r)
+        else:
+            assert(o == r)
+        return r
+
+TamarinruleParser.TamarinruleParser.TermContext.getText = termOverride
+
+
--- makeDH/pyVisit.py
+++ makeDH/pyVisit.py
@@ -5,6 +5,7 @@ from antlr4 import *
 from .TamarinruleLexer import TamarinruleLexer
 from .TamarinruleParser import TamarinruleParser
 from .makeDH import TamarinruleVisitor
+import makeDHParser
 
 def makeTree(input_stream):
 	lexer = TamarinruleLexer(input_stream)
