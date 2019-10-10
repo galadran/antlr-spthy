ab5ca732f4f68cef07dec7ec2f5f3a47a28c64ca
Dennis Jackson <dennis.jackson@cs.ox.ac.uk>
2019-10-10 14:21:25 +0100

    Lift transform to function

--- allTogether.py
+++ allTogether.py
@@ -3,10 +3,12 @@ from convertWhole import extractRules
 from antlr4 import InputStream
 import sys
 
-s = open(sys.argv[1],'r').read()
-(pt,rules) = extractRules(s)
-rulesT = convert(InputStream(rules))
+def transformSpthy(s):
+	(pt,rules) = extractRules(s)
+	rulesT = convert(InputStream(rules))
+	newpt = pt.replace('\n RULESUBHERE \n',str(rulesT))
+	return newpt
 
-newpt = pt.replace('\n RULESUBHERE \n',str(rulesT))
-
-print(newpt)
+if __name__=='__main__':
+	s = open(sys.argv[1],'r').read()
+	print(transformSpthy(s))
