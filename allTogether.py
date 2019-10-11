30301afcf7f47edf34c9c58552bfa0a125984ed4
Dennis Jackson <dennis.jackson@cs.ox.ac.uk>
2019-10-11 15:08:10 +0100

    Add error handling for the transformation

--- allTogether.py
+++ allTogether.py
@@ -2,10 +2,18 @@ from convert import convert
 from convertWhole import extractRules
 from antlr4 import InputStream
 import sys
+from traceback import print_exc
 
 def transformSpthy(s):
 	(pt,rules) = extractRules(s)
-	rulesT = convert(InputStream(rules))
+	try:
+		rulesT = convert(InputStream(rules))
+	except Exception as E:
+		print(rules)
+
+		print('---')
+		print_exc()
+		exit(-1)
 	newpt = pt.replace('\n RULESUBHERE \n',str(rulesT))
 	return newpt
 
